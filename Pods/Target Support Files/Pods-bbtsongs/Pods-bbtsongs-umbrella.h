78e857796d5c046aa4836b42ed4976f3246d3cee
Value solution <ves@values-mac-mini.local>
2017-12-05 12:56:25 +0530

    df

--- Pods/Target Support Files/Pods-bbtsongs/Pods-bbtsongs-umbrella.h
+++ Pods/Target Support Files/Pods-bbtsongs/Pods-bbtsongs-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_testTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_testTestsVersionString[];
+FOUNDATION_EXPORT double Pods_bbtsongsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_bbtsongsVersionString[];
 
